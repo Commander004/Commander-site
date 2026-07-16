c5875ca9b3e3326464025ab52b88d659fec42cfc
Commander004 <artin.golestani1391@gmail.com>
2026-07-16 22:59:01 +0330

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,40 @@
+// =========================
+// Commander Welcome Page
+// =========================
 
+// Get Data
+
+const username = localStorage.getItem("commander_username");
+const avatar = localStorage.getItem("commander_avatar");
+const memberID = localStorage.getItem("commander_memberid");
+
+// If profile doesn't exist
+
+if (!username || !avatar || !memberID) {
+
+    window.location.href = "profile.html";
+
+}
+
+// Elements
+
+const welcomeTitle = document.getElementById("welcomeTitle");
+const avatarImage = document.getElementById("avatar");
+const memberText = document.getElementById("memberID");
+const enterButton = document.getElementById("enterButton");
+
+// Show User Data
+
+welcomeTitle.textContent = `Welcome, ${username}!`;
+
+avatarImage.src = avatar;
+
+memberText.textContent = memberID;
+
+// Enter Website
+
+enterButton.addEventListener("click", () => {
+
+    window.location.href = "main.html";
+
+});
