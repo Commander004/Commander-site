928d347bab38d56670b9bf8fd2903c1c0907f1bd
Commander004 <artin.golestani1391@gmail.com>
2026-07-17 00:57:10 +0330

    Update game.js

--- game/the-maze/game.js
+++ game/the-maze/game.js
@@ -5,7 +5,9 @@ canvas.width = 800;
 canvas.height = 500;
 
 
+// =====================
 // WORLD
+// =====================
 
 const tileSize = 32;
 
@@ -43,13 +45,22 @@ const maze = [
 ];
 
 
+
+// =====================
 // PLAYER
+// =====================
 
 let player = {
+
     x:0,
     y:0,
+
     size:24,
-    speed:4
+
+    speed:4,
+
+    health:100
+
 };
 
 
@@ -71,199 +82,334 @@ for(let y=0;y<maze.length;y++){
 
 
 
-// CAMERA
+// =====================
+// ITEMS
+// =====================
 
-let camera={
-    x:0,
-    y:0
+let items=[
+
+    {
+        type:"battery",
+        x:12,
+        y:5
+    },
+
+    {
+        type:"medkit",
+        x:30,
+        y:15
+    },
+
+    {
+        type:"key",
+        x:45,
+        y:22
+    }
+
+];
+
+
+
+
+// =====================
+// MONSTER
+// =====================
+
+let monster={
+
+    x:900,
+
+    y:300,
+
+    size:26,
+
+    speed:1.2
+
 };
 
 
 
+
+
+// =====================
+// CAMERA
+// =====================
+
+let camera={
+
+    x:0,
+
+    y:0
+
+};
+
+
+
+
+
+// =====================
 // INPUT
+// =====================
 
 let keys={};
 
 
 window.addEventListener("keydown",e=>{
+
     keys[e.key.toLowerCase()]=true;
+
 });
 
 
 window.addEventListener("keyup",e=>{
+
     keys[e.key.toLowerCase()]=false;
+
 });
 
 
 
 
+
+// =====================
 // COLLISION
+// =====================
 
 function isWall(x,y){
 
+
     let col=Math.floor(x/tileSize);
+
     let row=Math.floor(y/tileSize);
 
 
-    if(!maze[row] || !maze[row][col])
+
+    if(!maze[row] || maze[row][col]===undefined)
+
         return true;
+
 
 
     return maze[row][col]=="#";
 
+
 }
+
 
 
 
 function canMove(x,y){
 
+
     return(
+
         !isWall(x,y) &&
+
         !isWall(x+player.size,y) &&
+
         !isWall(x,y+player.size) &&
+
         !isWall(x+player.size,y+player.size)
+
     );
+
 
 }
 
 
 
 
+
+// =====================
 // UPDATE
+// =====================
 
 function update(){
 
 
     let nx=player.x;
+
     let ny=player.y;
 
 
-    if(keys["w"]||keys["arrowup"])
+
+    if(keys["w"] || keys["arrowup"])
+
         ny-=player.speed;
 
 
-    if(keys["s"]||keys["arrowdown"])
+
+    if(keys["s"] || keys["arrowdown"])
+
         ny+=player.speed;
 
 
-    if(keys["a"]||keys["arrowleft"])
+
+    if(keys["a"] || keys["arrowleft"])
+
         nx-=player.speed;
 
 
-    if(keys["d"]||keys["arrowright"])
+
+    if(keys["d"] || keys["arrowright"])
+
         nx+=player.speed;
 
 
 
+
+
     if(canMove(nx,player.y))
+
         player.x=nx;
 
 
+
     if(canMove(player.x,ny))
+
         player.y=ny;
 
 
 
+
+
+    // Monster Follow
+
+    let dx=player.x-monster.x;
+
+    let dy=player.y-monster.y;
+
+
+    let distance=Math.sqrt(dx*dx+dy*dy);
+
+
+
+    if(distance<400){
+
+
+        monster.x += (dx/distance)*monster.speed;
+
+        monster.y += (dy/distance)*monster.speed;
+
+
+    }
+
+
+
+
+
+    // Camera
+
     camera.x=player.x-canvas.width/2;
+
     camera.y=player.y-canvas.height/2;
+
+
 
 }
 
 
 
 
+
+
+// =====================
 // DRAW
+// =====================
 
 function draw(){
 
 
     ctx.fillStyle="#080808";
 
+
     ctx.fillRect(
+
         0,
+
         0,
+
         canvas.width,
+
         canvas.height
+
     );
 
 
 
+
+
+    // MAP
+
+
     for(let y=0;y<maze.length;y++){
+
 
         for(let x=0;x<maze[y].length;x++){
 
 
+
             let sx=x*tileSize-camera.x;
+
             let sy=y*tileSize-camera.y;
 
 
 
-            // FLOOR
 
-            if(maze[y][x]!== "#"){
+
+            if(maze[y][x]!="#"){
+
 
                 ctx.fillStyle="#151515";
 
+
                 ctx.fillRect(
+
                     sx,
+
                     sy,
+
                     tileSize,
+
                     tileSize
+
                 );
 
-
-                // floor dots
-
-                ctx.fillStyle="#1c1c1c";
-
-                ctx.fillRect(
-                    sx+14,
-                    sy+14,
-                    3,
-                    3
-                );
 
             }
 
 
 
-            // WALL
+
+
 
             if(maze[y][x]=="#"){
 
-
-                ctx.shadowBlur=0;
 
 
                 ctx.fillStyle="#292929";
 
 
                 ctx.fillRect(
+
                     sx,
+
                     sy,
+
                     tileSize,
+
                     tileSize
+
                 );
 
-
-                ctx.strokeStyle="#3d3d3d";
-
-                ctx.lineWidth=1;
-
-
-                ctx.strokeRect(
-                    sx,
-                    sy,
-                    tileSize,
-                    tileSize
-                );
 
 
             }
 
 
 
-            // EXIT
+
+
 
             if(maze[y][x]=="E"){
 
@@ -271,15 +417,24 @@ function draw(){
                 ctx.fillStyle="#00eaff";
 
                 ctx.shadowBlur=10;
+
                 ctx.shadowColor="#00eaff";
 
 
                 ctx.fillRect(
+
                     sx,
+
                     sy,
+
                     tileSize,
+
                     tileSize
+
                 );
+
+
+                ctx.shadowBlur=0;
 
 
             }
@@ -291,43 +446,146 @@ function draw(){
 
 
 
+
+
+
+    // ITEMS
+
+
+    items.forEach(item=>{
+
+
+        let sx=item.x*tileSize-camera.x;
+
+        let sy=item.y*tileSize-camera.y;
+
+
+
+        if(item.type=="battery")
+
+            ctx.fillStyle="#ffe600";
+
+
+
+        if(item.type=="medkit")
+
+            ctx.fillStyle="#ff3333";
+
+
+
+        if(item.type=="key")
+
+            ctx.fillStyle="#00ffff";
+
+
+
+
+        ctx.fillRect(
+
+            sx,
+
+            sy,
+
+            20,
+
+            20
+
+        );
+
+
+
+    });
+
+
+
+
+
+
+
+    // MONSTER
+
+
+    ctx.fillStyle="#ff0055";
+
+
+    ctx.shadowBlur=15;
+
+    ctx.shadowColor="#ff0055";
+
+
+    ctx.fillRect(
+
+        monster.x-camera.x,
+
+        monster.y-camera.y,
+
+        monster.size,
+
+        monster.size
+
+    );
+
+
+
     ctx.shadowBlur=0;
+
+
+
+
 
 
 
     // PLAYER
 
+
     ctx.fillStyle="#a855f7";
 
+
     ctx.shadowBlur=12;
+
     ctx.shadowColor="#a855f7";
 
 
     ctx.fillRect(
+
         player.x-camera.x,
+
         player.y-camera.y,
+
         player.size,
+
         player.size
+
     );
 
 
     ctx.shadowBlur=0;
 
 
+
 }
 
 
 
 
+
+// =====================
+// LOOP
+// =====================
 
 function gameLoop(){
 
+
     update();
+
     draw();
+
 
     requestAnimationFrame(gameLoop);
 
+
 }
+
 
 
 gameLoop();
