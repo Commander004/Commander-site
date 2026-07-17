d7cc2a809b1cb051c5d105b2a33f3cab7569c5e4
Commander004 <artin.golestani1391@gmail.com>
2026-07-17 07:54:24 +0330

    Update monster.js

--- game/the-maze/js/monster.js
+++ game/the-maze/js/monster.js
@@ -1,1 +1,63 @@
+// =====================
+// MONSTER
+// =====================
 
+const monster = {
+
+    x: 900,
+    y: 300,
+
+    size: 26,
+
+    speed: 1.2,
+
+    health: 100
+
+};
+
+
+// =====================
+// UPDATE
+// =====================
+
+function updateMonster() {
+
+    const dx = player.x - monster.x;
+    const dy = player.y - monster.y;
+
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance < 400 && distance > 0) {
+
+        monster.x += (dx / distance) * monster.speed;
+        monster.y += (dy / distance) * monster.speed;
+
+    }
+
+}
+
+
+// =====================
+// DRAW
+// =====================
+
+function drawMonster() {
+
+    ctx.fillStyle = "#ff0055";
+
+    ctx.shadowBlur = 15;
+    ctx.shadowColor = "#ff0055";
+
+    ctx.fillRect(
+
+        monster.x - camera.x,
+        monster.y - camera.y,
+
+        monster.size,
+        monster.size
+
+    );
+
+    ctx.shadowBlur = 0;
+
+}
