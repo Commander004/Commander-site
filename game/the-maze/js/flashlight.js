8a401d676503944a2ea689f345478b22b78d1a80
Commander004 <artin.golestani1391@gmail.com>
2026-07-17 09:00:48 +0330

    Update flashlight.js

--- game/the-maze/js/flashlight.js
+++ game/the-maze/js/flashlight.js
@@ -1,10 +1,21 @@
 // =====================
-// FLASHLIGHT
+// FLASHLIGHT SYSTEM
 // =====================
 
 
 let flashlightOn = false;
 
+
+let battery = 100;
+
+
+let batteryDrain = 0.02;
+
+
+
+// =====================
+// TOGGLE
+// =====================
 
 
 function toggleFlashlight(){
@@ -13,9 +24,94 @@ function toggleFlashlight(){
     flashlightOn = !flashlightOn;
 
 
-    console.log(
-        "Flashlight:",
-        flashlightOn
+}
+
+
+
+// =====================
+// UPDATE
+// =====================
+
+
+function updateFlashlight(){
+
+
+    if(flashlightOn && battery > 0){
+
+
+        battery -= batteryDrain;
+
+
+    }
+
+
+
+    if(battery <= 0){
+
+
+        battery = 0;
+
+        flashlightOn = false;
+
+
+    }
+
+
+}
+
+
+
+// =====================
+// DRAW LIGHT
+// =====================
+
+
+function drawFlashlight(){
+
+
+    if(!flashlightOn)
+
+        return;
+
+
+
+    const gradient = ctx.createRadialGradient(
+
+        player.x - camera.x + player.size/2,
+
+        player.y - camera.y + player.size/2,
+
+        20,
+
+        player.x - camera.x + player.size/2,
+
+        player.y - camera.y + player.size/2,
+
+        180
+
+    );
+
+
+
+    gradient.addColorStop(0,"rgba(255,255,200,0.35)");
+
+    gradient.addColorStop(1,"rgba(0,0,0,0)");
+
+
+
+    ctx.fillStyle = gradient;
+
+
+    ctx.fillRect(
+
+        0,
+
+        0,
+
+        canvas.width,
+
+        canvas.height
+
     );
 
 
