ebdf7fa518d62569fbac00d564a0be0e9245f73a
Commander004 <artin.golestani1391@gmail.com>
2026-07-17 07:53:14 +0330

    Update camera.js

--- game/the-maze/js/camera.js
+++ game/the-maze/js/camera.js
@@ -1,1 +1,22 @@
+// =====================
+// CAMERA
+// =====================
 
+const camera = {
+
+    x: 0,
+    y: 0
+
+};
+
+
+// =====================
+// UPDATE CAMERA
+// =====================
+
+function updateCamera() {
+
+    camera.x = player.x - canvas.width / 2;
+    camera.y = player.y - canvas.height / 2;
+
+}
