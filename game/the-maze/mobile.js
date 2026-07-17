51c1c4bbcea732893d47e998d9e3c5ce8a06519b
Commander004 <artin.golestani1391@gmail.com>
2026-07-17 08:25:57 +0330

    Update mobile.js

--- game/the-maze/mobile.js
+++ game/the-maze/mobile.js
@@ -1,1 +1,105 @@
+// =====================
+// MOBILE CONTROLS
+// =====================
 
+
+// گرفتن دکمه ها
+
+const upBtn = document.getElementById("up");
+const downBtn = document.getElementById("down");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+
+
+
+// =====================
+// TOUCH START
+// =====================
+
+function pressKey(key){
+
+    keys[key] = true;
+
+}
+
+
+
+// =====================
+// TOUCH END
+// =====================
+
+function releaseKey(key){
+
+    keys[key] = false;
+
+}
+
+
+
+
+// =====================
+// BUTTON EVENTS
+// =====================
+
+
+upBtn.addEventListener("touchstart",()=>{
+
+    pressKey("arrowup");
+
+});
+
+
+upBtn.addEventListener("touchend",()=>{
+
+    releaseKey("arrowup");
+
+});
+
+
+
+
+downBtn.addEventListener("touchstart",()=>{
+
+    pressKey("arrowdown");
+
+});
+
+
+downBtn.addEventListener("touchend",()=>{
+
+    releaseKey("arrowdown");
+
+});
+
+
+
+
+leftBtn.addEventListener("touchstart",()=>{
+
+    pressKey("arrowleft");
+
+});
+
+
+leftBtn.addEventListener("touchend",()=>{
+
+    releaseKey("arrowleft");
+
+});
+
+
+
+
+rightBtn.addEventListener("touchstart",()=>{
+
+    pressKey("arrowright");
+
+});
+
+
+rightBtn.addEventListener("touchend",()=>{
+
+    releaseKey("arrowright");
+
+});
