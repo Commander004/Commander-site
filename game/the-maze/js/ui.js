f6a50a2d3a968e09d6086a2bf6926f41e54db6c5
Commander004 <artin.golestani1391@gmail.com>
2026-07-17 07:55:27 +0330

    Update ui.js

--- game/the-maze/js/ui.js
+++ game/the-maze/js/ui.js
@@ -1,1 +1,21 @@
+// =====================
+// UI
+// =====================
 
+function updateUI() {
+
+    // اینجا بعداً دکمه‌های موبایل،
+    // Backpack،
+    // Use،
+    // Shoot،
+    // Health Bar
+    // اضافه میشن.
+
+}
+
+
+function drawUI() {
+
+    drawInventory();
+
+}
