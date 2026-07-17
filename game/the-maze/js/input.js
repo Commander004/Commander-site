7c4a8234d2b006deb43ee8a80e2915b3401f1169
Commander004 <artin.golestani1391@gmail.com>
2026-07-17 07:52:29 +0330

    Update input.js

--- game/the-maze/js/input.js
+++ game/the-maze/js/input.js
@@ -1,1 +1,18 @@
+// =====================
+// INPUT
+// =====================
 
+const keys = {};
+
+window.addEventListener("keydown", (e) => {
+
+    keys[e.key.toLowerCase()] = true;
+
+});
+
+
+window.addEventListener("keyup", (e) => {
+
+    keys[e.key.toLowerCase()] = false;
+
+});
