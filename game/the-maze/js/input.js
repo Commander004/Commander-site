5524e198aa40a8f5de6bd19d7fabb4f9dc699908
Commander004 <artin.golestani1391@gmail.com>
2026-07-17 08:47:13 +0330

    Update input.js

--- game/the-maze/js/input.js
+++ game/the-maze/js/input.js
@@ -2,21 +2,44 @@
 // INPUT
 // =====================
 
+
 const keys = {};
 
-window.addEventListener("keydown", (e) => {
+
+
+window.addEventListener("keydown", (e)=>{
+
 
     keys[e.key.toLowerCase()] = true;
 
+
+
+    // Inventory Select
+
+    if(e.key >= "1" && e.key <= "9"){
+
+        selectSlot(Number(e.key) - 1);
+
+    }
+
+
+    if(e.key === "0"){
+
+        selectSlot(9);
+
+    }
+
+
+
 });
 
 
-window.addEventListener("keyup", (e) => {
+
+
+window.addEventListener("keyup",(e)=>{
+
 
     keys[e.key.toLowerCase()] = false;
 
-});
 
-window.addEventListener("keydown", (e) => {
-    console.log("Key:", e.key);
 });
